7df4f04660f3f7eb8469f9aee25f14eba9a919e5
gergopinter77-lgtm <gergopinter77@gmail.com>
2025-12-14 14:26:06 +0100

    mobile header done
    The entire header changed

---
+++ Nav_and_Flickity.js
@@ -0,0 +1,22 @@
+/*hamburger*/
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n =>n.
+addEventListener("clcik", () => {
+        hamburger.classList.remove("active")
+        .classList.remove("active")
+}))
+/*hamburger end*/ 
+
+var flickity = new Flickity('.carousel', {
+    cellAlign: 'left',
+    contain: true,
+})
+
